8cadbb3fc8f0ae13ba3d694dd58393a38852e479
Alfredo Di Napoli <alfredo@well-typed.com>
2022-06-13 12:51:00 +0200

    Switch vmstats to report megabytes

--- ridley-extras/src/System/Metrics/Prometheus/Ridley/Metrics/VirtualMemory.hs
+++ ridley-extras/src/System/Metrics/Prometheus/Ridley/Metrics/VirtualMemory.hs
@@ -20,17 +20,17 @@ import           Text.Read (readMaybe)
 
 {- Calling 'vmstat' will report
 
-[centos@atlas-eu ~]$ vmstat -s
-    130701440 K total memory
-     52971728 K used memory
-     92670416 K active memory
-     33348008 K inactive memory
-       852768 K free memory
-            0 K buffer memory
-     76876944 K swap cache
-            0 K total swap
-            0 K used swap
-            0 K free swap
+[centos@atlas-eu ~]$ vmstat -S M -s
+    130701440 M total memory
+     52971728 M used memory
+     92670416 M active memory
+     33348008 M inactive memory
+       852768 M free memory
+            0 M buffer memory
+     76876944 M swap cache
+            0 M total swap
+            0 M used swap
+            0 M free swap
     ...
 -}
 
@@ -39,18 +39,18 @@ getVmStats :: IO VmStatReport
 getVmStats = do
   rawOutput <- shelly $ silently $ escaping False $
     mapMaybe (readMaybe @Word64 . T.unpack) . T.words . T.strip
-      <$> run "vmstat" ["-s" ,"|" , "head", "-n", "10" , "|" , "awk", "-F", "\" \"" , "'{print $1}'" ]
+      <$> run "vmstat" ["-S", "M", "-s" ,"|" , "head", "-n", "10" , "|" , "awk", "-F", "\" \"" , "'{print $1}'" ]
   case rawOutput of
-    [   vmstat_total_memory_kb
-      , vmstat_used_memory_kb
-      , vmstat_active_memory_kb
-      , vmstat_inactive_memory_kb
-      , vmstat_free_memory_kb
-      , vmstat_buffer_memory_kb
-      , vmstat_swap_cache_kb
-      , vmstat_total_swap_kb
-      , vmstat_used_swap_kb
-      , vmstat_free_swap_kb
+    [   vmstat_total_memory_mb
+      , vmstat_used_memory_mb
+      , vmstat_active_memory_mb
+      , vmstat_inactive_memory_mb
+      , vmstat_free_memory_mb
+      , vmstat_buffer_memory_mb
+      , vmstat_swap_cache_mb
+      , vmstat_total_swap_mb
+      , vmstat_used_swap_mb
+      , vmstat_free_swap_mb
       ] -> pure $ VmStatReport{..}
     _ -> pure emptyVmStatReport
 
@@ -62,58 +62,58 @@ updateVmStat VmStatGauges{..} _ = do
 #else
   VmStatReport{..} <- getVmStats
 #endif
-  P.set (realToFrac vmstat_total_memory_kb    ) vmstat_total_memory_kb_g
-  P.set (realToFrac vmstat_used_memory_kb     ) vmstat_used_memory_kb_g
-  P.set (realToFrac vmstat_active_memory_kb   ) vmstat_active_memory_kb_g
-  P.set (realToFrac vmstat_inactive_memory_kb ) vmstat_inactive_memory_kb_g
-  P.set (realToFrac vmstat_free_memory_kb     ) vmstat_free_memory_kb_g
-  P.set (realToFrac vmstat_buffer_memory_kb   ) vmstat_buffer_memory_kb_g
-  P.set (realToFrac vmstat_swap_cache_kb      ) vmstat_swap_cache_kb_g
-  P.set (realToFrac vmstat_total_swap_kb      ) vmstat_total_swap_kb_g
-  P.set (realToFrac vmstat_used_swap_kb       ) vmstat_used_swap_kb_g
-  P.set (realToFrac vmstat_free_swap_kb       ) vmstat_free_swap_kb_g
+  P.set (realToFrac vmstat_total_memory_mb    ) vmstat_total_memory_mb_g
+  P.set (realToFrac vmstat_used_memory_mb     ) vmstat_used_memory_mb_g
+  P.set (realToFrac vmstat_active_memory_mb   ) vmstat_active_memory_mb_g
+  P.set (realToFrac vmstat_inactive_memory_mb ) vmstat_inactive_memory_mb_g
+  P.set (realToFrac vmstat_free_memory_mb     ) vmstat_free_memory_mb_g
+  P.set (realToFrac vmstat_buffer_memory_mb   ) vmstat_buffer_memory_mb_g
+  P.set (realToFrac vmstat_swap_cache_mb      ) vmstat_swap_cache_mb_g
+  P.set (realToFrac vmstat_total_swap_mb      ) vmstat_total_swap_mb_g
+  P.set (realToFrac vmstat_used_swap_mb       ) vmstat_used_swap_mb_g
+  P.set (realToFrac vmstat_free_swap_mb       ) vmstat_free_swap_mb_g
 
 data VmStatReport =
   VmStatReport {
-    vmstat_total_memory_kb    :: !Word64
-  , vmstat_used_memory_kb     :: !Word64
-  , vmstat_active_memory_kb   :: !Word64
-  , vmstat_inactive_memory_kb :: !Word64
-  , vmstat_free_memory_kb     :: !Word64
-  , vmstat_buffer_memory_kb   :: !Word64
-  , vmstat_swap_cache_kb      :: !Word64
-  , vmstat_total_swap_kb      :: !Word64
-  , vmstat_used_swap_kb       :: !Word64
-  , vmstat_free_swap_kb       :: !Word64
+    vmstat_total_memory_mb    :: !Word64
+  , vmstat_used_memory_mb     :: !Word64
+  , vmstat_active_memory_mb   :: !Word64
+  , vmstat_inactive_memory_mb :: !Word64
+  , vmstat_free_memory_mb     :: !Word64
+  , vmstat_buffer_memory_mb   :: !Word64
+  , vmstat_swap_cache_mb      :: !Word64
+  , vmstat_total_swap_mb      :: !Word64
+  , vmstat_used_swap_mb       :: !Word64
+  , vmstat_free_swap_mb       :: !Word64
   } deriving (Show, Eq)
 
 emptyVmStatReport :: VmStatReport
 emptyVmStatReport = VmStatReport
-  { vmstat_total_memory_kb    = 0
-  , vmstat_used_memory_kb     = 0
-  , vmstat_active_memory_kb   = 0
-  , vmstat_inactive_memory_kb = 0
-  , vmstat_free_memory_kb     = 0
-  , vmstat_buffer_memory_kb   = 0
-  , vmstat_swap_cache_kb      = 0
-  , vmstat_total_swap_kb      = 0
-  , vmstat_used_swap_kb       = 0
-  , vmstat_free_swap_kb       = 0
+  { vmstat_total_memory_mb    = 0
+  , vmstat_used_memory_mb     = 0
+  , vmstat_active_memory_mb   = 0
+  , vmstat_inactive_memory_mb = 0
+  , vmstat_free_memory_mb     = 0
+  , vmstat_buffer_memory_mb   = 0
+  , vmstat_swap_cache_mb      = 0
+  , vmstat_total_swap_mb      = 0
+  , vmstat_used_swap_mb       = 0
+  , vmstat_free_swap_mb       = 0
   }
 
 
 data VmStatGauges =
   VmStatGauges {
-    vmstat_total_memory_kb_g    :: !P.Gauge
-  , vmstat_used_memory_kb_g     :: !P.Gauge
-  , vmstat_active_memory_kb_g   :: !P.Gauge
-  , vmstat_inactive_memory_kb_g :: !P.Gauge
-  , vmstat_free_memory_kb_g     :: !P.Gauge
-  , vmstat_buffer_memory_kb_g   :: !P.Gauge
-  , vmstat_swap_cache_kb_g      :: !P.Gauge
-  , vmstat_total_swap_kb_g      :: !P.Gauge
-  , vmstat_used_swap_kb_g       :: !P.Gauge
-  , vmstat_free_swap_kb_g       :: !P.Gauge
+    vmstat_total_memory_mb_g    :: !P.Gauge
+  , vmstat_used_memory_mb_g     :: !P.Gauge
+  , vmstat_active_memory_mb_g   :: !P.Gauge
+  , vmstat_inactive_memory_mb_g :: !P.Gauge
+  , vmstat_free_memory_mb_g     :: !P.Gauge
+  , vmstat_buffer_memory_mb_g   :: !P.Gauge
+  , vmstat_swap_cache_mb_g      :: !P.Gauge
+  , vmstat_total_swap_mb_g      :: !P.Gauge
+  , vmstat_used_swap_mb_g       :: !P.Gauge
+  , vmstat_free_swap_mb_g       :: !P.Gauge
   }
 
 --------------------------------------------------------------------------------
@@ -123,14 +123,14 @@ systemVirtualMemory :: MonadIO m
                     -> P.RegistryT m RidleyMetricHandler
 systemVirtualMemory opts = do
   let popts = opts ^. prometheusOptions
-  gauges <- VmStatGauges <$> P.registerGauge "vmstat_total_memory_kb" (popts ^. labels)
-                         <*> P.registerGauge "vmstat_used_memory_kb" (popts ^. labels)
-                         <*> P.registerGauge "vmstat_active_memory_kb" (popts ^. labels)
-                         <*> P.registerGauge "vmstat_inactive_memory_kb" (popts ^. labels)
-                         <*> P.registerGauge "vmstat_free_memory_kb" (popts ^. labels)
-                         <*> P.registerGauge "vmstat_buffer_memory_kb" (popts ^. labels)
-                         <*> P.registerGauge "vmstat_swap_cache_kb" (popts ^. labels)
-                         <*> P.registerGauge "vmstat_total_swap_kb" (popts ^. labels)
-                         <*> P.registerGauge "vmstat_used_swap_kb" (popts ^. labels)
-                         <*> P.registerGauge "vmstat_free_swap_kb" (popts ^. labels)
+  gauges <- VmStatGauges <$> P.registerGauge "vmstat_total_memory_mb" (popts ^. labels)
+                         <*> P.registerGauge "vmstat_used_memory_mb" (popts ^. labels)
+                         <*> P.registerGauge "vmstat_active_memory_mb" (popts ^. labels)
+                         <*> P.registerGauge "vmstat_inactive_memory_mb" (popts ^. labels)
+                         <*> P.registerGauge "vmstat_free_memory_mb" (popts ^. labels)
+                         <*> P.registerGauge "vmstat_buffer_memory_mb" (popts ^. labels)
+                         <*> P.registerGauge "vmstat_swap_cache_mb" (popts ^. labels)
+                         <*> P.registerGauge "vmstat_total_swap_mb" (popts ^. labels)
+                         <*> P.registerGauge "vmstat_used_swap_mb" (popts ^. labels)
+                         <*> P.registerGauge "vmstat_free_swap_mb" (popts ^. labels)
   return $ mkRidleyMetricHandler "ridley-virtual-memory-statistics" gauges updateVmStat False
